2b4679a3218f7a1eca142bfb4ddc046af407e0f7
Abel Barrera Duran <abelbarrera15@gmail.com>
2021-04-27 19:46:43 -0400

    part review listing done

---
+++ part_reviews.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!isset($_COOKIE['user_name'])) {
+    header("HTTP/1.0 404 Not Found");
+    echo '<h1>This page was not found</h1>';
+    echo '<h1>You do not have access. Please login</h1>';
+    die();
+}
+
+else {
+    include 'db_connection.php';
+    $part_id = $_GET["id"];
+    $sql = "SELECT P_Rating_ID, P_Rating_Rating, P_Rating_Title, P_Rating_Comment FROM P_RATING WHERE Part_Id = cast('$part_id' as int)";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $fin_ech = '<table border="1" cellspacing="0" cellpadding="0" width="800" align="center"><tr><th>Rating_User</th><th>Rating</th><th>Title</th><th>Comment</th><tr>';
+            while($row = mysqli_fetch_assoc($result)) {
+                $fin_ech = $fin_ech."<tr><td>".$row['P_Rating_ID']."</td><td>".$row['P_Rating_Rating']."</td><td>".$row['P_Rating_Title']."</td><td>".$row['P_Rating_Comment']."</td></tr>";
+            }
+    } 
+    else {
+        echo "No results";
+    }
+    echo $fin_ech."</table>";
+    }
+
+?>
